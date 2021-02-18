ded4abe20494b08f5d8f23fc72c3d6cf32a02f45
Chris Ference <cference8@gmail.com>
2021-02-17 20:44:30 -0600

    Initial commit

---
+++ ApplicationForm/src/test/java/com/sg/ApplicationForm/ApplicationFormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sg.ApplicationForm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationFormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
